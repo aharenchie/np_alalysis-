dfea3ae3b81f381a2372064c300783199d38bd04
阿波連　智恵 <k168582@ie.u-ryukyu.ac.jp>
2017-06-21 22:26:38 +0900

    fix main

--- main.py
+++ main.py
@@ -1,155 +1,67 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import sys
 import csv
-import re
-import types
-import MeCab
+import run_knp 
 
-"""
-1.辞書の読み込み
-in : なし
-out : 評価極性辞書 dic
-
-やること：DB機能の実装
-"""
-def import_pn_data():
-
-    pn_dic ={}
-
-    # 1-2.日本語評価極性辞書(用語編)
-    with open("/Users/e125731/workspace/m1/dic/wago.121808.pn",'r') as f:
-        for pn_wago in f.readlines():
-            pn_wago_list = pn_wago.split('\t')
-        
-            # 評価用言の変換
-            pn_wago_list[1] = pn_wago_list[1].replace(" ","").replace('\n','')
-
-            # ネガポジの数値化
-            value_list = pn_wago_list[0].split('（')
-            if value_list[0] == "ポジ":
-                value = 1 #ポジ
-            else:
-                value = -1 #ネガ
-
-            # 辞書に追加
-            pn_dic[pn_wago_list[1]] = value
-
-
-
-    # 1-3.日本語評価極性辞書(名詞編)
-    with open("/Users/e125731/workspace/m1/dic/pn.csv.m3.120408.trim",'r') as f:
-
-        # ネガポジの数値化
-        for pn_noun in f.readlines():
-            pn_noun_list = pn_noun.split('\t')
-            if pn_noun_list[1] == "p":
-                value = 1 #ポジ
-            elif pn_noun_list[1] == "n":
-                value = -1 #ネガ
-            elif pn_noun_list[1] == "e":
-                value = 0 #ニュートラル
-
-            # 辞書に追加
-            pn_dic[pn_noun_list[0]] = value
-
-    return(pn_dic)
-
-"""
-2.レビューデータ形態素解析
-in : 1件のレビューデータ str
-out : 1件のレビューデータの分かち書き list
-
-やること：ストップワードの追加
-"""
-def import_review_data(text):
-    word_list = []
-    mt = MeCab.Tagger("-Ochasen -d /usr/local/Cellar/mecab/0.996/lib/mecab/dic/mecab-ipadic-neologd")
-    mt.parse('')
-    node = mt.parseToNode(text)
-    
-    while node:
-        if node.feature.split(",")[0] != "BOS/EOS":
-            word_list.append(node.surface)
-        node = node.next
-
-    return(word_list)
-
-"""
- 3.文章（単語リスト）に対する評価極性値を返す。
-in : 1件のレビューデータ list
-out : 1件レビューデータ評価極性値 int, 評価極性語 list
-
-
-"""
-def cal_review_pn(word_list):
-    p_word=[]
-    n_word=[]
-    e_word=[]
-    score = 0
-    
-    for word in word_list:
-        
-        if word in pn_dic:
-            score += pn_dic[word]
-            
-            if pn_dic[word] == 1:
-                p_word.append(word)
-            elif pn_dic[word] == 0:
-                e_word.append(word)
-            elif pn_dic[word] == -1:
-                n_word.append(word)
-
-    return(score,p_word,e_word,n_word)
-            
 
 if __name__ == "__main__":
 
     argvs = sys.argv
     argc = len(argvs)
 
+    bnst_dic = {}
+    bnst_order_dic = {}
+
+    # 評価対象語リストの用意
+    pn_word_list = []
+    pn_id = 0
+    
+    bnst_left = []
+    bnst_right = []
+    
+    # 引数チェック
     if (argc != 2):
         print ('prease type "python %s reviewfile"'%argvs[0])
         sys.exit()
 
-    # 辞書の読み込み
-    pn_dic = import_pn_data()
-    
     # レビューの読み込み
     f = open(argvs[1])
     dataReader_review = csv.reader(f)
 
-    compare_data = {}
-
-    # レビュー評価計算
-    for review in dataReader_review:
-
-        user_score = re.search(r'[0-9]+',review[1]).group()
-        text = review[2]
-
-        # レビューデータの形態素解析
-        word_list = import_review_data(text)
-        # 評価極性値の計算
-        score,p_word,e_word,n_word = cal_review_pn(word_list)
-
-        print(text)        
-        print("")
-        print("ユーザー評価 予想評価")
-        print("{0}            {1}".format(user_score,score))
-        print("ポジティブ単語:{0}".format(p_word))
-        print("ネガティブ単語:{0}".format(n_word))
-        print("ニュートラル単語:{0}".format(e_word))
-        print("---------------------------")
-
-        compare_data.setdefault(user_score, []).append(score)
-
-    # 比較出力用
-    for key, value in compare_data.items():
-        print("ユーザー評価 予想評価")
-        print("{0}            {1}".format(key,value))
-
-         
+    for review in dataReader_review:        
+        li=review[3].split(",")
+        for i in li:
+            print(i)
+        """
+        # レビューデータに、評価対象語があるか判断
         
-            
+
+        # レビューデータに、評価対象語があったら実行
+        if flag:
+            # 評価対象語ごとに実行。
+            # 構文解析格納・係り先順取得と評価対処語のid検索を別枠でやるようにする！
+            for pn_word in pn_word_list:
+                # KNPによる構文解析
+                #bnst_dic,pn_id = run_knp.save_bnst(pn_word,text)
+
+                # 文節ごとに、係り先順を取得
+                #bnst_order_dic = run_knp.get_bnst_order(bnst_dic)
+
+        
+                # ターゲット単語が受けるものを取得
+                #bnst_left = run_knp.get_bnst_left(bnst_dic,bnst_order_dic,pn_id)
+    
+                # ターゲット単語が係るものを取得
+                #bnst_right = run_knp.get_bnst_right(bnst_dic,bnst_order_dic,pn_id)
+    
+                # 出力
+                print("ターゲット単語: {0}".format(pn_word))
+                print("入力文章: {0}".format(text))
+                print()
+                print("ターゲット単語が受けるもの(左)：{0}".format(bnst_left))
+                print("ターゲット単語が係るもの(右): {0}".format(bnst_right))
+                print("-----------------------")
+    
+        """
