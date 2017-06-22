785b8bed8309aae5ee3647dee40a1a9d617a80b6
阿波連　智恵 <k168582@ie.u-ryukyu.ac.jp>
2017-06-22 19:19:50 +0900

    fix func

--- main.py
+++ main.py
@@ -3,65 +3,56 @@
 
 import sys
 import csv
-import run_knp 
+import re
+import run_knp
+import prepare_word
 
+argvs = sys.argv
+argc = len(argvs)
+
+# 引数チェック
+if (argc != 3):
+    print ('prease type "python %s reviewfile keywordfile"'%argvs[0])
+    sys.exit()
 
 if __name__ == "__main__":
 
-    argvs = sys.argv
-    argc = len(argvs)
-
-    bnst_dic = {}
-    bnst_order_dic = {}
-
-    # 評価対象語リストの用意
-    pn_word_list = []
-    pn_id = 0
-    
-    bnst_left = []
-    bnst_right = []
-    
-    # 引数チェック
-    if (argc != 2):
-        print ('prease type "python %s reviewfile"'%argvs[0])
-        sys.exit()
-
-    # レビューの読み込み
+    # レビューファイルの読み込み
     f = open(argvs[1])
-    dataReader_review = csv.reader(f)
+    Reader_review_data = csv.reader(f)
+    
+    # キャラクター・キャスト・映画スタッフの読み込み
+    f = open(argvs[2])
+    Reader_keyword_data = csv.reader(f)
+    keyword_data = [row for row in Reader_keyword_data]
+    
+    for i,review_data in enumerate(Reader_review_data):
 
-    for review in dataReader_review:        
-        li=review[3].split(",")
-        for i in li:
-            print(i)
-        """
-        # レビューデータに、評価対象語があるか判断
+        print(i+1)
         
+        review_text=review_data[3].split(",")  #レビューデータ(評価、本文等)から本文取り出し        
 
-        # レビューデータに、評価対象語があったら実行
-        if flag:
-            # 評価対象語ごとに実行。
-            # 構文解析格納・係り先順取得と評価対処語のid検索を別枠でやるようにする！
-            for pn_word in pn_word_list:
-                # KNPによる構文解析
-                #bnst_dic,pn_id = run_knp.save_bnst(pn_word,text)
+        for text in review_text:
+            keyword_list = prepare_word.choose_keyword(text,keyword_data)
 
-                # 文節ごとに、係り先順を取得
-                #bnst_order_dic = run_knp.get_bnst_order(bnst_dic)
+            if len(keyword_list) != 0:
+                
+                for keyword in keyword_list:
+                    bnst_dic,keyword_id = run_knp.save_bnst(keyword,text)
+                
+                    # 文節ごとに、係り先順を取得
+                    bnst_order_dic = run_knp.get_bnst_order(bnst_dic)
 
-        
-                # ターゲット単語が受けるものを取得
-                #bnst_left = run_knp.get_bnst_left(bnst_dic,bnst_order_dic,pn_id)
+                    # ターゲット単語が受けるものを取得
+                    bnst_left = run_knp.get_bnst_left(bnst_dic,bnst_order_dic,keyword_id)
+            
+                    # ターゲット単語が係るものを取得
+                    bnst_right = run_knp.get_bnst_right(bnst_dic,bnst_order_dic,keyword_id)
     
-                # ターゲット単語が係るものを取得
-                #bnst_right = run_knp.get_bnst_right(bnst_dic,bnst_order_dic,pn_id)
-    
-                # 出力
-                print("ターゲット単語: {0}".format(pn_word))
-                print("入力文章: {0}".format(text))
-                print()
-                print("ターゲット単語が受けるもの(左)：{0}".format(bnst_left))
-                print("ターゲット単語が係るもの(右): {0}".format(bnst_right))
-                print("-----------------------")
-    
-        """
+                    # 出力
+                    print("ターゲット単語: {0}".format(keyword))
+                    print("入力文章: {0}".format(text))
+                    print()
+                    print("ターゲット単語が受けるもの(左)：{0}".format(bnst_left))
+                    print("ターゲット単語が係るもの(右): {0}".format(bnst_right))
+                    print("-----------------------")        
---
+++ prepare_word.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import re
+
+def choose_keyword(text, keyword_data):
+
+    keyword_list = []
+    for keywords in keyword_data:
+        for keyword in keywords:
+            list= re.split("・|\s",keyword)            
+            for t in list:
+                if t in text:
+                    keyword_list.append(t)
+                    
+    return keyword_list
